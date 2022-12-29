26707f8b8a6a677bc7f76a1572a02280fffaf6ad
Ivan Ristic <ivanr@webkreator.com>
2022-12-29 11:16:45 +0000

    Refined the audit log schema.

--- schema/src/main/com/qlued/pg/schema/V6__Tenant_audit_log.sql
+++ schema/src/main/com/qlued/pg/schema/V6__Tenant_audit_log.sql
@@ -1,216 +1,71 @@
-/*
-
- Use cases:
-
- - Use as an event log, for example to record being able or not being able to do something.
-
- - Record key user activity, such as authentication, password change, and so on.
-
- - Record actions taken against resources.
-
- - Support user service accounts.
-
- - Record system activity.
-
- - Record activity of support staff that are external to the tenant.
-
- */
-
--- TODO In our design, user accounts are separate from tenants. What this means is that
---      user activity doesn't belong to the tenants. Although this gives us flexibility to
---      support some interesting use cases (e.g., consultants and support staff), it
---      also creates problems. For example, it's a reasonable to expect to see authentication
---      failures associated with a corporate user. How do we resolve that? One way might
---      be to hav a separate user_audit log and to copy key authentication data into
---      tenant_audit_log. The idea being that joining an org also gives permission to the
---      org to monitor security events.
---
--- TODO Another problem is figuring out what to do with user accounts that are created
---      with the sole purpose of accessing a single tenant, when their access is revoked,
---      or when the tenant is cancelled.
-
-CREATE TABLE tenant_audit_log
+CREATE TABLE audit_log
 (
-    tenant_id                      UUID        NOT NULL REFERENCES tenants (tenant_id) ON DELETE CASCADE,
 
-    event_id                       UUID        NOT NULL DEFAULT gen_chrono_uuid(),
+    entry_id        UUID        NOT NULL DEFAULT gen_chrono_uuid() PRIMARY KEY,
 
-    PRIMARY KEY (tenant_id, event_id),
+    -- Optional, but required for entries that take place within tenant accounts.
+    tenant_id       UUID REFERENCES tenants (tenant_id) ON DELETE CASCADE,
 
-
-    -- Actor information.
-
-    -- Who is the principal? For example: tenant user, external/third-party user
-    -- (e.g., support staff), system, and anonymous activity.
-    principal_type                 TEXT        NOT NULL, -- TODO Enum.
-
-    -- Activity of support staff is recorded via their own identities, which are
-    -- created on the fly. Such tenant user records probably shouldn't identify
-    -- the staff, but show as their organization instead. In the UI we probably
-    -- want to show just one meta tenant user, rather than the individual users.
-
-    tenant_user_id                 UUID, -- REFERENCES tenant_users (tenant_user_id),
-
-    -- To keep track of users' service accounts, for example for CLI and API access.
-    tenant_user_service_account_id UUID,
-
-    -- Used to track the identity of the external/third-party organization for
-    -- audit logs created for external users such as support staff.
-    external_tenant_id             UUID REFERENCES tenants (tenant_id) ON DELETE RESTRICT,
+    -- Used to give tenants access to entries. Normally, all tenants have access to
+    -- events related to their accounts. However, they may also get access to other
+    -- events, for example those related to user accounts that are considered to
+    -- belong to them. Row level security operates based on this value.
+    authz_tenant_id UUID        NOT NULL REFERENCES tenants (tenant_id) ON DELETE CASCADE,
 
 
-    -- Core event metadata, such as time and access location.
+    -- Principal information.
 
-    timestamp                      TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    -- An easy way to determine principal types:
+    --   'A' - anonymous
+    --   'S' - system
+    --   'U' - user
+    principal_type  TEXT        NOT NULL,
 
-    -- Tracks means of interaction with the platform. In most cases this
-    -- will be "app" or "api", but also other components where that makes sense.
-    origin                         TEXT        NOT NULL, -- TODO Enum.
+    -- Optional, but required for activities carried out by users.
+    user_id         UUID REFERENCES users (user_id) ON DELETE RESTRICT,
 
-    remote_addr                    TEXT,                 -- TODO Better type.
+    -- For use when users act on behalf of an organization, for example, member
+    -- of support staff making changes for the tenant.
+    proxy_tenant_id UUID REFERENCES tenants (tenant_id) ON DELETE RESTRICT,
 
-    session_id                     TEXT,
 
-    device_id                      TEXT,
+    -- Core event metadata.
 
-    -- Unique transaction/request ID, where applicable. Usually generated
-    -- at the edge, for example a CDN, reverse proxy, or web server.
-    transaction_id                 TEXT,
+    timestamp       TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+
+    -- Unique transaction/request ID, where applicable. The idea is that
+    -- every activity is initiated somewhere and we want to be able to
+    -- track an activity to its origin.
+    transaction_id  TEXT,
+
+    -- Optional, if there is an IP address associated with this entry.
+    remote_addr     INET,
+
+    -- Optional, if there is user session associated with this entry.
+    session_id      TEXT,
+
+    -- Optional, if there is a device associated with this entry.
+    device_id       TEXT,
 
 
     -- Information about the activity itself.
 
-    -- For HTTP events, contains the URL path, including the query string.
-    resource                       TEXT,
+    -- Unique resource identifier. For HTTP events, contains the URL path, including the query string.
+    resource        TEXT        NOT NULL,
 
     -- For example, 'http.get' or 'user.auth.signed_in'.
-    action                         TEXT,
+    activity        TEXT        NOT NULL,
 
     -- Tracks whether the action was successful, using HTTP status codes.
-    status                         TEXT,                 -- TODO Enum
+    status          INTEGER     NOT NULL,
 
-    duration                       INTERVAL,
-
-    severity                       TEXT        NOT NULL, -- TODO Enum
-
-    -- Optional, human-readable description of the event.
-    description                    TEXT,
+    severity        TEXT        NOT NULL, -- TODO Enum?
 
 
-    -- Additional custom data associated with the event.
+    -- Custom data associated with the event.
 
-    attachment                     JSONB,
+    attachment_type TEXT,                 -- TODO Must not be NULL if attachment is not NULL.
 
-    attachment_type                TEXT                  -- TODO Must not be NULL if attachment is not NULL.
+    attachment      JSONB
+
 );
-
--- TODO Row-level security. Tenants can only read the access logs.
-
--- TODO Writing done on a separate connection and a different role. No updates or deletes (permissions + triggers), timestamp enforced.
-
--- TODO Partition the tenant_audit_log table.
-
--- TODO Fix data types and add limits.
-
-/*
-
-Events
-------
-
-user.lifecycle:
-
- requested
- invited
- invite_accepted
- created
- disabled
- enabled
- requested_deletion
- deletion_refused
- deleted_auto
- deleted_admin
-
-user.auth
-
- signed_in
- signed_out
- new_device
- new_ip -- emitted every time a new IP address is associated with a session; it's more complicated
-           to detect new IP addresses if stateless auth is used, but one could keep a list of
-           IP addressed observed recently and emit events based on that.
-
- password_success
- password_failed
- mfa_hotp_success
- mfa_hotp_failed
- backup_code_success
- backup_code_failed
- rme_success
- rme_failed
- network_success
- network_failed
-
- -- auth: password, 2fa, rme + network access list
-
- auth_blocked
- auth_refused
-
- -- todo: general purpose anomaly indicator
-
- password_changed
- password_changed_admin
- password_reset_requested
- password_reset_failed
- password_changed_reset
-
- mfa_enabled
- mfa_disabled
- mfa_disabled_admin
- mfa_codes_generated
- mfa_codes_viewed
-
- email_change_requested
- email_change_cancelled
- email_change_confirmed
-
-
-
- joined_tenant
- left_tenant
- -- tenant_permissions
-
-
- tenant_authz_successful
- -- tenant_authz_failed: mfa, network restrictions
-
-
-tenant:
-
- created
- disabled
- enabled
- deleted
-
- mfa_enabled
- mfa_disabled
-
- -- TODO Network access control configuration updated
- -- TODO Owner/Admin access added and removed
-
- user_invited
- user_declined
- user_joined
- user_left
- user_disabled
- user_enabled
- user_removed
- user_disconnected
- user_access_requested
- user_access_approved
- user_access_refused
-
- user_session_authorized
-
- forbidden action
-
- */
