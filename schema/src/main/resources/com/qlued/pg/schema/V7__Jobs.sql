0d7c9c2846d8bb7b6509a2a360ed766bb5748565
Ivan Ristic <ivanr@webkreator.com>
2024-04-27 09:08:13 +0100

    Docs.

--- schema/src/main/resources/com/qlued/pg/schema/V7__Jobs.sql
+++ schema/src/main/resources/com/qlued/pg/schema/V7__Jobs.sql
@@ -1,15 +1,35 @@
--- A simple jobs table that uses partitioning to reduce bloat. There
--- is partitioning at two levels: first based on status, where we have
--- one partition for active and completed jobs, and another for dead
--- jobs. The former is partitioned further into daily partitions. The
--- dead letter table is not partitioned based on time.
---
--- The idea is that we may want to drop the table with good jobs on
--- a daily basis or something similar, but we may want to keep the
--- dead jobs for potentially longer. We don't want dead jobs to
--- be preventing us from deleting the bulk of the data.
+/*
+
+A simple jobs table that uses partitioning to reduce contention and
+bloat. There is partitioning at three levels: job type, status, and time.
+
+At the first level, the idea is to have one jobs table, but different
+partitions for different job types, or queues. In this example, one
+partition is used for two queues, and there is a separate partition
+for the third queue. With multiple queues it's possible to use
+multiple job pullers without locking. And even if locking is used,
+there will be less contention on a per queue basis.
+
+If multiple pullers per queue are needed, the table could be further
+sharded. A system of leases can be devised to let pullers compete
+and that all shards are handled in the event of puller failure.
+
+In Postgres 17, merging and splitting of partitions will be supported,
+which will make it easier to use queues. You could start with one
+default queue and split as needed.
+
+We want to use the next level of partitioning to reduce bloat, with
+daily (for example) partitions that can be quickly dropped. However,
+we still use further two partitioning levels in order to support
+a dead letter queue, the idea being that we sometimes may want to
+keep dead jobs for longer. Thus the latter two partition layers
+are per job status, then run_at timestamp.
+
+ */
 
 CREATE TYPE job_status AS ENUM ('active', 'completed', 'dead');
+
+CREATE TYPE job_type AS ENUM ('queue1', 'queue2', 'queue3');
 
 CREATE TABLE tenant_jobs
 (
@@ -21,13 +41,15 @@ CREATE TABLE tenant_jobs
 
     run_at     TIMESTAMPTZ NOT NULL DEFAULT now(),
 
+    type       JOB_TYPE    NOT NULL,
+
     input      JSONB       NOT NULL,
 
     status     JOB_STATUS  NOT NULL DEFAULT 'active',
 
-    PRIMARY KEY (tenant_id, job_id, status, run_at)
+    PRIMARY KEY (tenant_id, job_id, type, status, run_at)
 
-) PARTITION BY LIST (status);
+) PARTITION BY LIST (type);
 
 -- This index is designed to support finding new jobs added since
 -- a timestamp. The idea is that, at startup, one puller will load
@@ -52,18 +74,34 @@ GRANT ALL
 --      need to create sufficient future partitions depending on how far in the
 --      future run_at can be.
 
-CREATE TABLE tenant_jobs_undead PARTITION OF tenant_jobs FOR VALUES IN ('active', 'completed') PARTITION BY RANGE (run_at);
+CREATE TABLE tenant_jobs_default PARTITION OF tenant_jobs FOR VALUES IN ('queue1','queue2') PARTITION BY LIST (status);
 
-CREATE TABLE tenant_jobs_dead PARTITION OF tenant_jobs FOR VALUES IN ('dead');
+CREATE TABLE tenant_jobs_queue3 PARTITION OF tenant_jobs FOR VALUES IN ('queue3') PARTITION BY LIST (status);
+
+CREATE TABLE tenant_jobs_default_undead PARTITION OF tenant_jobs_default FOR VALUES IN ('active', 'completed') PARTITION BY RANGE (run_at);
+
+CREATE TABLE tenant_jobs_default_dead PARTITION OF tenant_jobs_default FOR VALUES IN ('dead');
+
+CREATE TABLE tenant_jobs_queue3_undead PARTITION OF tenant_jobs_queue3 FOR VALUES IN ('active', 'completed') PARTITION BY RANGE (run_at);
+
+CREATE TABLE tenant_jobs_queue3_dead PARTITION OF tenant_jobs_queue3 FOR VALUES IN ('dead');
 
 -- TODO Do we need to configure row-level security on the partitions?
 
 /*
 -- Use partman to create future partitions. This assumes we'll
 -- create jobs that run only up to 30 days in the future.
-SELECT partman.create_parent(p_parent_table => 'main.tenant_jobs_undead',
+
+SELECT partman.create_parent(p_parent_table => 'main.tenant_jobs_default_undead',
                              p_control => 'run_at',
                              p_interval => '1 day',
                              p_premake => '33'
        );
+
+SELECT partman.create_parent(p_parent_table => 'main.tenant_jobs_queue3_undead',
+                             p_control => 'run_at',
+                             p_interval => '1 day',
+                             p_premake => '33'
+       );
+
 */
